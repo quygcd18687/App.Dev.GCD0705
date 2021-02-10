68bbe4348c4b276ad850b809a710412af47ab8a0
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-02-10 19:13:11 +0700

    Create CourseController and make function add courses

---
+++ TTMS/Controllers/CourseController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMS.Models;
+
+namespace TMS.Controllers
+{
+
+
+	public class CourseController : Controller
+	{
+
+		private ApplicationDbContext _context;
+		public CourseController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		// GET: Course
+		public ActionResult Index()
+		{
+			return View(_context.Courses.ToList());
+		}
+
+		// Add Course
+		[HttpGet]
+
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Course course)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			var newCourse = new Course()
+			{
+				Name = course.Name,
+				Description = course.Description,
+				Categogy = course.Categogy
+			};
+
+			_context.Courses.Add(newCourse);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+	}
+}
