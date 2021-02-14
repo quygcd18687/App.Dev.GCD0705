7b83922a1c896f8ff8228c98ce81daab4a939d7c
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-02-14 22:15:54 +0700

    Create,Edit and Delete course

--- TTMS/Controllers/CourseController.cs
+++ TTMS/Controllers/CourseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TMS.Models;
+using TTMS.ViewModels;
 
 namespace TMS.Controllers
 {
@@ -23,16 +24,22 @@ namespace TMS.Controllers
 			return View(_context.Courses.ToList());
 		}
 
-		// Add Course
-		[HttpGet]
 
-
+		/// <summary>
+		/// This function is used to create a new course
+		/// </summary>
+		/// <returns></returns>
 		public ActionResult Create()
 		{
-			return View();
+			var viewModel = new CourseCategoriesViewModel()
+			{
+				Categories = _context.Categories.ToList()
+			};
+			return View(viewModel);
 		}
 
-		[HttpPost]
+		[HttpPost]		
+
 		public ActionResult Create(Course course)
 		{
 			if (!ModelState.IsValid)
@@ -42,11 +49,62 @@ namespace TMS.Controllers
 			var newCourse = new Course()
 			{
 				Name = course.Name,
-				Description = course.Description,
+				Description = course.Description,				
 				Categogy = course.Categogy
 			};
 
 			_context.Courses.Add(newCourse);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+
+
+		/// <summary>
+		/// This function is used for course editing
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpGet]
+		public ActionResult Edit(int id)
+		{
+			var courseInDb = _context.Courses.SingleOrDefault(t => t.Id == id);
+
+			if (courseInDb == null) return HttpNotFound();
+
+			return View(courseInDb);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Course course)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			var InforCourseInDb = _context.Courses.SingleOrDefault(t => t.Id == course.Id);
+
+			if (InforCourseInDb == null) return HttpNotFound();
+
+			var courseInDb = _context.Courses.SingleOrDefault(t => t.Id == course.Id);
+
+			courseInDb.Name = course.Name;
+			courseInDb.Description = course.Description;
+			courseInDb.Categogy = course.Categogy;
+
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Delete(int id)
+
+		{
+
+			var courseInDb = _context.Courses.SingleOrDefault(t => t.Id == id );
+
+			if (courseInDb == null) return HttpNotFound();
+
+			_context.Courses.Remove(courseInDb);
 			_context.SaveChanges();
 			return RedirectToAction("Index");
 		}
